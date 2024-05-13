687a85eb47b72d5f48f07841be65c85f5286f26b
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-05-13 22:35:20 +0100

    new commits

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -12,6 +12,29 @@ const First = () => {
         <h2>DELIVERY STATUS</h2>
         <div className="flex flex-row">
           <h5>In Transit </h5>
+          <h5 className="mt-4">TRACKING ID </h5>
+          <div className="flex flex-row">
+            <h6>785005280643</h6>
+          </div>
+
+          <div>
+            <p>FROM</p>
+            <p>LONG BEACH, CA US</p>
+            <p>Label Created</p>
+            <p>5/3/7073 8•31 AM</p>
+
+            <p>PACKAGE RECEIVED BY FEDEX</p>
+            <p>SANTA FE SPRINGS, CA</p>
+            <p>5/3/2023 12:00 AM</p>
+
+            <p>IN TransIT</p>
+            <p>KANSAS CITY. MO</p>
+            <p>5/5/7073 10:05 PM</p>
+
+            <p>OUT FOR DELIVERY</p>
+            <p>TO</p>
+            <p>SAINTIOUIS MO US</p>
+          </div>
         </div>
       </div>
     </div>
