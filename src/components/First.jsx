7c839da6e7653e046c5722ef1ea406122bd9015c
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-05-13 22:28:59 +0100

    new commits

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const First = () => {
   return (
     <div>
-   <h1>hii</h1>
+      <div className="flex flex-col">
+        <h5>UPDATED DELIVERY</h5>
+        <h1>MONDAY</h1>
+        <h5>13/05/2024 by end of today</h5>
+        <h6>Initially expected: Tuesday, 5/9/2023</h6>
+        <h6>Early</h6>
+        <h2>DELIVERY STATUS</h2>
+        <div className="flex flex-row">
+          <h5>In Transit </h5>
+        </div>
+      </div>
     </div>
   );
 };
